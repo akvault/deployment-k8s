81a4898fb9f126853f8275f741b3c04b498dba14
akvault <akndreams@gmail.com>
2021-08-11 03:14:52 +0530

    Create enable-alb.sh

---
+++ alb-loadbalancer/enable-alb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+kubectl apply -f alb-loadbalancer/aws-load-balancer-controller-service-account.yaml
+
+kubectl apply \
+    --validate=false \
+    -f https://github.com/jetstack/cert-manager/releases/download/v1.1.1/cert-manager.yaml
+
+
+curl -o v2_2_0_full.yaml https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.2.0/docs/install/v2_2_0_full.yaml
+
+kubectl apply -f v2_2_0_full.yaml
